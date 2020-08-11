9bafcb16d9f82e73987f7569eb162fa5a1878801
vdmytriukhin <vdmytriukhin@foundationmedicine.com>
2020-08-11 19:18:31 +0300

    initial commit

---
+++ src/main/java/lesson3/HW_Example.java
@@ -0,0 +1,34 @@
+package lesson3;
+
+public class HW_Example {
+    public static void main(String[] args) {
+        //***
+        //***
+        //***
+        int length = 5;
+        while (length > 0) {
+            int jLength = 5;
+            while (jLength > 0) {
+                System.out.print("*");
+                jLength--;
+            }
+            System.out.println();
+            length--;
+        }
+
+        int length2 = 5;
+        do{
+            int jLength2 = 5;
+            do {
+                System.out.print("-");
+                jLength2--;
+            } while (jLength2 > 0);
+            System.out.println();
+            length2--;
+        } while (length2 > 0);
+
+        //2.14.1098 ["2", "14", "1098"]
+        //2.14.10
+        //3.1.2
+    }
+}
