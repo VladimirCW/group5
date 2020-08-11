26cac79738a87c793ca70570491aef7b1def18a5
vdmytriukhin <vdmytriukhin@foundationmedicine.com>
2020-08-11 22:05:24 +0300

    initial commit

---
+++ src/main/java/lesson3/HW_For_Unit.java
@@ -0,0 +1,15 @@
+package lesson3;
+
+public class HW_For_Unit {
+    public String someMethod() {
+        String result = "";
+        String name = "*";
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                result += name;
+            }
+            result += "\n";
+        }
+        return result;
+    }
+}
---
+++ src/test/java/lesson4/TestCat.java
@@ -0,0 +1,22 @@
+package lesson4;
+
+public class TestCat {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Barsik");
+        int age = 105;
+        cat.setAge(age);
+        int actualAge = cat.getAge();
+        int expectedAge = age;
+        if(actualAge != expectedAge) {
+            throw new Error("Validation failed");
+        }
+
+        int age2 = 2;
+        cat.setAge(age2);
+        int actualAge2 = cat.getAge();
+        int expectedAge2 = age2;
+        if(actualAge2 != expectedAge2) {
+            throw new Error("Validation failed");
+        }
+    }
+}
