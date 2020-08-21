9e3266a99636729a4c9a92ccf77929977f2f9522
vdmytriukhin <vdmytriukhin@foundationmedicine.com>
2020-08-21 21:53:51 +0300

    dsadsa

--- src/test/java/lesson6/HWTest.java
+++ src/test/java/lesson6/HWTest.java
@@ -1,18 +1,25 @@
 package test.java.lesson6;
 
 import main.java.HW.Task1;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class HWTest {
 
-    @Test
-    public void test1() {
+    @Test(dataProvider = "dp")
+    public void test1(int arr[], int expected) {
         Task1 task1 = new Task1();
-        int arr[] = {4,5,-2,0,8};
         int actual = task1.min(arr);
-        int expected = -2;
         assertEquals(actual, expected);
+    }
+
+    @DataProvider(name = "dp")
+    public Object[][] provider() {
+        return new Object[][] {
+                {new int[] {1,2,3,4}, 1},
+                {new int[] {1,2,-3,4}, -3}
+        };
     }
 }
